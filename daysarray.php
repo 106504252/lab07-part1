fb3cac558f678b5d393bdec91ca0b656bb555d30
106504252 <106504252@student.swin.edu.au>
2026-04-23 14:01:42 +1000

    task 2 implemented

---
+++ daysarray.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP Variables, Arrays and Operators</h1>
+
+<?php
+    $days = array [Monday, Tuesday, Wednesday, Thursday, Friday]
+    echo "<p>$days</p>"
+?>
+</body>
+</html>
