69ed1fff0ebd2c56cdaddc1fdb0dac94111d3aab
106504252 <106504252@student.swin.edu.au>
2026-04-23 14:20:45 +1000

    french days added

--- daysarray.php
+++ daysarray.php
@@ -10,8 +10,11 @@
 
 <?php
     $days = array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-    echo "<p>They days of the week in English are:</p>";
-    echo "$days[0], $days[1], $days[2], $days[3], $days[4], $days[5], $days[6]"
+    echo "<p>The days of the week in English are:</p>";
+    echo "$days[0], $days[1], $days[2], $days[3], $days[4], $days[5], $days[6].";
+    $daysf = array ("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
+    echo "<p>The days of the week in French are:</p>";
+    echo "$daysf[0], $daysf[1], $daysf[2], $daysf[3], $daysf[4], $daysf[5], $daysf[6].";
 ?>
 </body>
 </html>
