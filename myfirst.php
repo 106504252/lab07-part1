344d03512b6f26d379c2d4b97d47d32799a46d3b
106504252 <106504252@student.swin.edu.au>
2026-04-23 13:52:40 +1000

    step 1 implmented

---
+++ myfirst.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using PHP Variables, arrays and operators</title>
+</head>
+<body>
+    <h1>PHP Variables, arrays and operators</h1>
+<?php
+    $marks = array (85, 85, 95); //declaring and initializing an array
+    $marks[1] = 90; //modifying second element
+    $ave = ($marks[0] + $marks[1] + $marks[2]) / 3; //compute average
+    if($ave >= 50)
+        $status = "PASSED";
+    else 
+        $status = "FAILED";
+    echo <p>the average score is $ave. You $status</p>;
+?>
+</body>
+</html>
